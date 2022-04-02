0e68b25c123edeb4aa155c2055b6bc60ea2d54d2
artemicdev <69939984+artemicdev@users.noreply.github.com>
2022-04-02 23:44:38 +0100

    ✨ Add .psc file-to-run argument
    Add the ability to run main.py with "-f" or "-file" argument, to specify a .psc file to run

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
 from ps2.app import PS2 as PS2
 
+if len(sys.argv) > 2:
+  if sys.argv[1] == "-file" or sys.argv[1] == "-f":
+    try:
+      PS2.runFile(sys.argv[2])
+    except IndexError:
+      print("Invalid or blank file path provided - provide path relative to exec dir")
+
 if __name__ == "__main__":
     PS2.runPrompt()
