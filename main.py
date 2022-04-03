fc7191d6b3fb3e4ad1d9aaa87735a04f50aa74fd
artemicdev <69939984+artemicdev@users.noreply.github.com>
2022-04-03 14:18:05 +0100

    🚸 Allow -f anywhere in arguments list
    Allow the -f/-file arg to be called at any position, not just in first.
    File path is obtained relative to -f arg: file path MUST be specified immediately after -f/-file.

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 from ps2.app import PS2 as PS2
 
 if len(sys.argv) > 1:
-  if sys.argv[1] == "-file" or sys.argv[1] == "-f":
+  if "-f" in sys.argv or "-file" in sys.argv:
     try:
-      PS2.runFile(sys.argv[2])
+      ind = (sys.argv).index("-f")
+    except ValueError:
+      ind = (sys.argv).index("-file")
+    try:
+      PS2.runFile(sys.argv[ind+1])
     except IndexError:
       print("File argument was blank, check syntax")
 
