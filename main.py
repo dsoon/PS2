4bc0c9b6b192dd2ccabd60851bca2f48379bf60a
artemicdev <69939984+artemicdev@users.noreply.github.com>
2022-04-03 16:09:41 +0100

    🎨 Re-implement arguments using argparse

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import sys
+import argparse
 from ps2.app import PS2 as PS2
 
-if len(sys.argv) > 1:
-  if "-f" in sys.argv or "-file" in sys.argv:
-    try:
-      ind = (sys.argv).index("-f")
-    except ValueError:
-      ind = (sys.argv).index("-file")
-    try:
-      PS2.runFile(sys.argv[ind+1])
-    except IndexError:
-      print("File argument was blank, check syntax")
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="specify .psc file to run")
+args = parser.parse_args()
+
+if args.file:
+  PS2.runFile(args.file)
 
 if __name__ == "__main__":
     PS2.runPrompt()
